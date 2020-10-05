6a05680d6cf17a8859ea018cb5186f16f22d91d4
unknown <csmezba@gmail.com>
2020-10-05 13:31:51 +0600

    Initial Commit

--- Controller/signup.js
+++ Controller/signup.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const request = require("request");
+const bodyParser = require("body-parser");
 const https = require("https");
 
 const app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + '/signup.html')
+  res.render("home");
 })
 
 app.post("/", (req, res) => {
+
   // From signup Html
   const fn = req.body.fname;
   const ln = req.body.lname;
   const mail = req.body.email;
   const pNumber = req.body.phone;
 
-// Check here -- https://mailchimp.com/developer/api/marketing/lists/batch-subscribe-or-unsubscibe/
+  // Check here -- https://mailchimp.com/developer/api/marketing/lists/batch-subscribe-or-unsubscibe/
   const data = {
     members: [{
       email_address: mail,
@@ -32,7 +33,7 @@ app.post("/", (req, res) => {
     }]
   };
 
-// The JSON.stringify() method converts a JavaScript object or value to a JSON string.
+  // The JSON.stringify() method converts a JavaScript object or value to a JSON string.
   const jsonData = JSON.stringify(data);
 
   const url = "https://us2.api.mailchimp.com/3.0/lists/6372cea042";
@@ -42,20 +43,20 @@ app.post("/", (req, res) => {
     auth : "leomezba:79efa2cce0d950b2091f376a059de6f7-us2"
   }
 
-// Check here -- https://nodejs.org/api/https.html#https_https_request_options_callback
+  // Check here -- https://nodejs.org/api/https.html#https_https_request_options_callback
   const request = https.request(url, options, (response) => {
     if(response.statusCode === 200)
     {
-      res.sendFile(process.cwd() + "/success.html");
+      res.render("success");
     }
     else
     {
-      res.sendFile(process.cwd() + "/failure.html");
+      res.render("failure");
     }
-    response.on("data", (data) => {
-      console.log(JSON.parse(data));
-      console.log(response.statusCode);
-    })
+    // response.on("data", (data) => {
+    //   console.log(JSON.parse(data));
+    //   console.log(response.statusCode);
+    // })
   })
 
   request.write(jsonData);
